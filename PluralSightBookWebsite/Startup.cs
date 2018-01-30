9a8ce5491761a0e8e54853c54f7b16244a756952
david <david@computhought.com.au>
2018-01-30 14:51:46 +0800

    Add project files.

---
+++ PluralSightBookWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PluralSightBookWebsite.Startup))]
+namespace PluralSightBookWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
